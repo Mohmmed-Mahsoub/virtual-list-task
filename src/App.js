d75640be6488e828194aba8fb84f8a0fae145a05
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2022-09-14 14:26:24 +0200

    render a 10,000 fake data and calclate the render time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">app</div>;
+  const [count, setCount] = useState(10000);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // repopulate the list when count changes
+    setItems(
+      new Array(count)
+        .fill(null)
+        .map((_, i) => ({ index: i, name: `Item ${i}` }))
+    );
+  }, []);
+
+  return (
+    <div className="page">
+      <div className="scroll">
+        <div className="inner">
+          {items.map((i) => (
+            <div key={i.name} className="item">
+              <label>{i.name}</label>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
