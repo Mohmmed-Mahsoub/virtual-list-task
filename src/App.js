df10719cba39c5ce31e91e856f74321405024465
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2022-09-14 16:17:27 +0200

    feat: make a very simple demo to implement the virtual list strategy for rendering with fixed number  for window Height

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(10000);
+  const [count, setCount] = useState(100);
   const [items, setItems] = useState([]);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const numItems = items.length;
+  const itemHeight = 40;
+  const innerHeight = numItems * itemHeight;
+  const windowHeight = 400;
+  const startIndex = Math.floor(scrollTop / itemHeight);
+  const endIndex = Math.min(
+    numItems - 1, // don't render past the end of the list
+    Math.floor((scrollTop + windowHeight) / itemHeight)
+  );
+
+  const onScroll = (e) => {
+    setScrollTop(e.currentTarget.scrollTop);
+  };
+
+  const renderItems = [];
+  for (let i = startIndex; i <= endIndex; i++) {
+    renderItems.push(items[i]);
+  }
 
   useEffect(() => {
-    // repopulate the list when count changes
     setItems(
       new Array(count)
         .fill(null)
@@ -16,10 +35,21 @@ function App() {
 
   return (
     <div className="page">
-      <div className="scroll">
-        <div className="inner">
+      <div
+        className="scroll"
+        style={{ overflowY: "scroll", height: `${windowHeight}px` }}
+        onScroll={onScroll}
+      >
+        <div
+          className="inner"
+          style={{ position: "relative", height: `${innerHeight}px` }}
+        >
           {items.map((i) => (
-            <div key={i.name} className="item">
+            <div
+              key={i.name}
+              className="item"
+              style={{ height: `${itemHeight}px` }}
+            >
               <label>{i.name}</label>
             </div>
           ))}
