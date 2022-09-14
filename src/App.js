a5308159bebc545081702cf7bfc0fa3ea2148484
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2022-09-14 16:41:33 +0200

    feat: render the virtual list depend on the acual window height

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import useWindowDimensions from "./customHooks/useWindowDimensions";
 
 function App() {
   const [count, setCount] = useState(100);
   const [items, setItems] = useState([]);
   const [scrollTop, setScrollTop] = useState(0);
+  const { windowHeight } = useWindowDimensions();
 
   const numItems = items.length;
   const itemHeight = 40;
   const innerHeight = numItems * itemHeight;
-  const windowHeight = 400;
+
   const startIndex = Math.floor(scrollTop / itemHeight);
   const endIndex = Math.min(
     numItems - 1, // don't render past the end of the list
