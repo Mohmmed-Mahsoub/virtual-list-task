3a93e20aab56ea11d5c95074e3dc774d5d56c8cb
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2022-09-16 06:44:34 +0200

    adjust UI: add custom modal close btn

--- src/components/modalComp/modalComp.component.js
+++ src/components/modalComp/modalComp.component.js
@@ -1,13 +1,24 @@
 import { Modal } from "react-bootstrap";
 import AddItemForm from "../addItemForm/addItemForm.component";
 import EditItemForm from "../editItemForm/editItemForm.component";
+import { AiOutlineClose } from "react-icons/ai";
 
 const ModalComp = ({ show, handleClose, modal, setEditItem }) => {
   const { modalType, modalData, modalTitle } = modal;
   return (
     <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title>{modalTitle}</Modal.Title>
+        <button
+          type="button"
+          className={`modal-close_btn p-2`}
+          aria-label="Close"
+          onClick={() => handleClose()}
+        >
+          <i className="d-flex justify-content-center align-items-center">
+            <AiOutlineClose />
+          </i>
+        </button>
       </Modal.Header>
       <Modal.Body>
         {/* decide with modal body content (component) will be render dynamicly depend on the modalType value and add the modalData for this content component which it will be need */}
